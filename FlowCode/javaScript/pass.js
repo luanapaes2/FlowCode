878b1212d303558a37c41d03a5c442a9c4df931b
luanapaes <114548504+luanapaes@users.noreply.github.com>
2023-07-04 20:51:24 -0300

    add
    adicionando sidebar à tela de arquivos

--- FlowCode/javaScript/pass.js
+++ FlowCode/javaScript/pass.js
@@ -6,23 +6,39 @@ function Entrar() {
   window.location.href = "tela-arquivos.html";
 }
 
-document.getElementById('olho').addEventListener('mousedown', function() {
-document.getElementById('pass').type = 'text';
+document.getElementById("olho").addEventListener("mousedown", function () {
+  document.getElementById("pass").type = "text";
 });
 
-document.getElementById('olho').addEventListener('mouseup', function() {
-document.getElementById('pass').type = 'password';
+document.getElementById("olho").addEventListener("mouseup", function () {
+  document.getElementById("pass").type = "password";
 });
 
 // Para que o password não fique exposto apos mover a imagem.
-document.getElementById('olho').addEventListener('mousemove', function() {
-document.getElementById('pass').type = 'password';
+document.getElementById("olho").addEventListener("mousemove", function () {
+  document.getElementById("pass").type = "password";
 });
 
-
-
 //abre hamburguer
-//---- adicionar função do sidebar
+
+//abrir sidebar
+function abrirSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.style.left = "0";
+}
+
+const openBtn = document.getElementById("openBtn");
+openBtn.addEventListener("click", abrirSidebar);
+
+// Fechar sidebar
+function fecharSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.style.left = "-300px";
+}
+
+const closeBtn = document.getElementById("closeBtn");
+closeBtn.addEventListener("click", fecharSidebar);
+
 
 //muda a cor da estrela
 function changeColor(button) {
@@ -30,19 +46,19 @@ function changeColor(button) {
 }
 
 //abre opções - descrição dos cards
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   var button = document.querySelector(".btnOptions");
   var options = button.querySelector(".options");
 
-  button.addEventListener("click", function() {
-      options.style.display = options.style.display === "none" ? "block" : "none";
+  button.addEventListener("click", function () {
+    options.style.display = options.style.display === "none" ? "block" : "none";
   });
 
-  options.addEventListener("click", function(event) {
-      event.stopPropagation(); // Impede que o clique nas opções feche o menu
+  options.addEventListener("click", function (event) {
+    event.stopPropagation(); // Impede que o clique nas opções feche o menu
   });
 
-  document.addEventListener("click", function() {
-      options.style.display = "none"; // Fecha o menu quando qualquer área fora do menu é clicada
+  document.addEventListener("click", function () {
+    options.style.display = "none"; // Fecha o menu quando qualquer área fora do menu é clicada
   });
 });
